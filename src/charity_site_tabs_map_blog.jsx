49aaad0d19eb1268f5df019a91fbcabde0c8c6be
james <s241691@dtu.dk>
2026-05-17 12:42:46 -0230

    gps link

--- src/charity_site_tabs_map_blog.jsx
+++ src/charity_site_tabs_map_blog.jsx
@@ -1717,13 +1717,13 @@ function CorsaTrackerEmbed() {
         /* iPhone / small screens: move Corsa page upward more */
         @media (max-width: 640px) {
           .corsa-frame {
-            height: 350px;
+            height: 330px;
           }
 
           .corsa-iframe {
           width: 220%;
           height: 900px;
-          transform: translate(-50%, -260px);
+          transform: translate(-50%, -275px);
         }
         }
 
@@ -1731,7 +1731,7 @@ function CorsaTrackerEmbed() {
         @media (max-width: 390px) {
           .corsa-iframe {
             width: 260%;
-            transform: translate(-50%, -260px);
+            transform: translate(-50%, -275px);
           }
         }
       `}</style>
