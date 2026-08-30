4b00fecb5cba4d6604ead7c038c3bc454ed7b21a
james <s241691@dtu.dk>
2026-08-29 20:17:44 -0600

    daily update

--- src/charity_site_tabs_map_blog.jsx
+++ src/charity_site_tabs_map_blog.jsx
@@ -1374,8 +1374,8 @@ function RunCounters({
 
   return (
     <div className="mt-6 grid gap-4 sm:grid-cols-3">
-      <StatCard label={t("stats.daysCompleted")} value={103} suffix={` / ${totalGoalDays}`} />
-      <StatCard label={t("stats.kilometersRun")} value={6584.83} suffix={t("stats.km")} />
+      <StatCard label={t("stats.daysCompleted")} value={104} suffix={` / ${totalGoalDays}`} />
+      <StatCard label={t("stats.kilometersRun")} value={6645.83} suffix={t("stats.km")} />
       <StatCard label={t("stats.amountRaised")} value={amountRaised} prefix={t("stats.currency")} />
     </div>
   );
